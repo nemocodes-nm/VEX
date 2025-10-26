13444374f057d4e84ef7f455dce829285327a727
nemocodes-nm <nimaisaimallipeddi824@gmail.com>
2025-10-26 17:06:24 -0400

    Update robot-config.cpp

--- robot-config.cpp
+++ robot-config.cpp
@@ -4,7 +4,13 @@ using namespace vex;
 
 // A global instance of brain used for printing to the V5 brain screen
 brain Brain;
+controller Controller1 = controller(primary)
+motor Motor1 = motor(PORT1, ratio18_1, false);
+motor Motor11 = motor(PORT11, ratio18_1, false);
+motor Motor10 = motor(PORT10, ratio18_1, false);
+motor Motor20 = motor(PORT20, ratio18_1, false);
 
+bool RemoteControlCodeEnabled = true;
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
  *
